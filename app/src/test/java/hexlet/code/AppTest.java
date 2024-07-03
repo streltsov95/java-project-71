9eb3e30e379f4a8ac19df6b3e8c96cf57a3b3c45
Viktor Streltsov <streltsov95va@gmail.com>
2024-07-03 21:00:06 +0300

    Add temp test

---
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -0,0 +1,13 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    void main() {
+        assertEquals(1, 1);
+    }
+}
