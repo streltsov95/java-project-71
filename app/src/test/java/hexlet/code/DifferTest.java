96c0067e409b1b034997855da9e48e7cba82e51f
Viktor Streltsov <streltsov95va@gmail.com>
2024-07-15 20:10:57 +0300

    Add Differ test

---
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -0,0 +1,53 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+class DifferTest {
+
+    private static String expectedStylish;
+//    private static String expectedJson;
+//    private static String expectedPlain;
+
+    private static Path getAbsolutePath(String fileName) {
+        return Paths.get("src", "test", "resources", fileName)
+                .toAbsolutePath().normalize();
+    }
+
+    private static String readFile(String fileName) throws Exception {
+        Path filePath = getAbsolutePath(fileName);
+        return Files.readString(filePath);
+    }
+
+    @BeforeAll
+    public static void beforeAll() throws Exception {
+//        String expectedJson = readFile("expected_results/json.json");
+//        String expectedPlain = readFile("expected_results/plain.txt");
+        expectedStylish = readFile("expected_results/stylish.txt");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"json", "yml"})
+    public void testGenerateWithDefaultOutput(String inputFormat) throws Exception {
+        String filePath1 = getAbsolutePath("input_files/file1." + inputFormat).toString();
+        String filePath2 = getAbsolutePath("input_files/file2." + inputFormat).toString();
+
+        String actualResult = Differ.generate(filePath1, filePath2);
+        assertEquals(expectedStylish, actualResult);
+    }
+    @ParameterizedTest
+    @ValueSource(strings = {"json", "yml"})
+    public void testGenerateWithStylishOutput(String inputFormat) throws Exception {
+        String filePath1 = getAbsolutePath("input_files/file1." + inputFormat).toString();
+        String filePath2 = getAbsolutePath("input_files/file2." + inputFormat).toString();
+
+        String actualResult = Differ.generate(filePath1, filePath2, "stylish");
+        assertEquals(expectedStylish, actualResult);
+    }
+}
