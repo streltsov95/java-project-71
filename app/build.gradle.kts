b6f4baf1af4fdb290fc5ae553afafa75164d4ed4
Viktor Streltsov <streltsov95va@gmail.com>
2024-07-15 20:08:47 +0300

    Add JUnit dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,9 @@ dependencies {
     implementation ("info.picocli:picocli:4.7.6")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.17.2")
     testImplementation(platform("org.junit:junit-bom:5.11.0-M2"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.3")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.11.0-M2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 application {
