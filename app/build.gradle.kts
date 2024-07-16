11b2b5232611e52b19682da59216bfed31f8ab01
Viktor Streltsov <streltsov95va@gmail.com>
2024-07-16 16:47:55 +0300

    Add jackson-yaml library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,7 @@ repositories {
 dependencies {
     implementation ("info.picocli:picocli:4.7.6")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.17.2")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.17.2")
     testImplementation(platform("org.junit:junit-bom:5.11.0-M2"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.3")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.11.0-M2")
