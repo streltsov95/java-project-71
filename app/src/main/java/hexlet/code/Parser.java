08e4beab7890cf0637d26a6a87146be1bbdecca7
Viktor Streltsov <streltsov95va@gmail.com>
2024-07-16 17:48:08 +0300

    Add YAML parser

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -2,6 +2,7 @@ package hexlet.code;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,10 +16,11 @@ public class Parser {
         if (format.equals("json")) {
             ObjectMapper objectMapper = new ObjectMapper();
             map = objectMapper.readValue(content, new TypeReference<Map<String, Object>>() {});
+        } else if (format.equals("yaml") || format.equals("yml")) {
+            ObjectMapper objectMapper = new YAMLMapper();
+            map = objectMapper.readValue(content, new TypeReference<Map<String, Object>>() {});
         }
-        
+
         return map;
-
     }
-
 }
