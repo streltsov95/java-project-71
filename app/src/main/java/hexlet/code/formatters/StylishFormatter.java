1e82e847faab4366a33abdd86e851dde5b88f7ba
Viktor Streltsov <streltsov95va@gmail.com>
2024-07-24 16:33:39 +0300

    Refactor format()

--- app/src/main/java/hexlet/code/formatters/StylishFormatter.java
+++ app/src/main/java/hexlet/code/formatters/StylishFormatter.java
@@ -5,42 +5,38 @@ import java.util.Map;
 
 public class StylishFormatter {
 
+    private static final String SAME_TEMPLATE = "    %s: %s\n";
+    private static final String REMOVED_TEMPLATE = "  - %s: %s\n";
+    private static final String ADDED_TEMPLATE = "  + %s: %s\n";
+    private static final String UPDATED_TEMPLATE = "  - %s: %s\n  + %s: %s\n";
+
     public static String format(List<Map<String, Object>> compareResult) {
+
         StringBuilder sb = new StringBuilder();
         sb.append("{\n");
+
         for (Map<String, Object> map : compareResult) {
-            if (map.get("STATUS").equals("SAME")) {
-                sb.append("    ");
-                sb.append(map.get("FIELD"));
-                sb.append(": ");
-                sb.append(map.get("VALUE"));
-                sb.append("\n");
-            }
-            if (map.get("STATUS").equals("REMOVED")) {
-                sb.append("  - ");
-                sb.append(map.get("FIELD"));
-                sb.append(": ");
-                sb.append(map.get("OLD_VALUE"));
-                sb.append("\n");
-            }
-            if (map.get("STATUS").equals("ADDED")) {
-                sb.append("  + ");
-                sb.append(map.get("FIELD"));
-                sb.append(": ");
-                sb.append(map.get("NEW_VALUE"));
-                sb.append("\n");
-            }
-            if (map.get("STATUS").equals("UPDATED")) {
-                sb.append("  - ");
-                sb.append(map.get("FIELD"));
-                sb.append(": ");
-                sb.append(map.get("OLD_VALUE"));
-                sb.append("\n");
-                sb.append("  + ");
-                sb.append(map.get("FIELD"));
-                sb.append(": ");
-                sb.append(map.get("NEW_VALUE"));
-                sb.append("\n");
+
+            String status = String.valueOf(map.get("STATUS"));
+            switch (status) {
+                case "SAME" -> {
+                    String same = String.format(SAME_TEMPLATE, map.get("FIELD"), map.get("VALUE"));
+                    sb.append(same);
+                }
+                case "REMOVED" -> {
+                    String removed = String.format(REMOVED_TEMPLATE, map.get("FIELD"), map.get("OLD_VALUE"));
+                    sb.append(removed);
+                }
+                case "ADDED" -> {
+                    String added = String.format(ADDED_TEMPLATE, map.get("FIELD"), map.get("NEW_VALUE"));
+                    sb.append(added);
+                }
+                case "UPDATED" -> {
+                    String updated = String.format(UPDATED_TEMPLATE,
+                            map.get("FIELD"), map.get("OLD_VALUE"), map.get("FIELD"), map.get("NEW_VALUE"));
+                    sb.append(updated);
+                }
+                default -> sb.append("");
             }
         }
         sb.append("}");
