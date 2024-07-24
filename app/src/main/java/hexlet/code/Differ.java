27919267df61cc9867e993e71d5d217a04bc86fa
Viktor Streltsov <streltsov95va@gmail.com>
2024-07-24 13:57:50 +0300

    Fix readFile(), rename Class

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -7,11 +7,11 @@ public class Differ {
 
     public static String generate(String filePath1, String filePath2, String outFormat) throws Exception {
 
-        String content1 = Reader.readFile(filePath1);
-        String fileType1 = Reader.getFileType(filePath1);
+        String content1 = Utils.readFile(filePath1);
+        String fileType1 = Utils.getFileType(filePath1);
 
-        String content2 = Reader.readFile(filePath2);
-        String fileType2 = Reader.getFileType(filePath2);
+        String content2 = Utils.readFile(filePath2);
+        String fileType2 = Utils.getFileType(filePath2);
 
         Map<String, Object> file1 = Parser.parse(content1, fileType1);
         Map<String, Object> file2 = Parser.parse(content2, fileType2);
--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -4,16 +4,10 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class Reader {
+public class Utils {
 
     public static String readFile(String filePath) throws Exception {
-
         Path path = Paths.get(filePath).toAbsolutePath().normalize();
-
-        if (!Files.exists(path)) {
-            throw new Exception("File '" + path + "' does not exist!");
-        }
-
         return Files.readString(path);
     }
 
