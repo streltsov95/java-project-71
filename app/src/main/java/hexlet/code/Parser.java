4993b7b25b9484b627fb7520a4b30554ea3e6990
Viktor Streltsov <streltsov95va@gmail.com>
2024-07-24 15:11:38 +0300

    Refactor parse()

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -4,23 +4,21 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class Parser {
 
     public static Map<String, Object> parse(String content, String format) throws Exception {
-
-        Map<String, Object> map = new HashMap<>();
-
-        if (format.equals("json")) {
-            ObjectMapper objectMapper = new ObjectMapper();
-            map = objectMapper.readValue(content, new TypeReference<Map<String, Object>>() { });
-        } else if (format.equals("yaml") || format.equals("yml")) {
-            ObjectMapper objectMapper = new YAMLMapper();
-            map = objectMapper.readValue(content, new TypeReference<Map<String, Object>>() { });
+        switch (format) {
+            case "json" -> {
+                ObjectMapper objectMapper = new ObjectMapper();
+                return objectMapper.readValue(content, new TypeReference<>() { });
+            }
+            case "yaml", "yml" -> {
+                ObjectMapper objectMapper = new YAMLMapper();
+                return objectMapper.readValue(content, new TypeReference<>() { });
+            }
+            default -> throw new RuntimeException("input format is not supported");
         }
-
-        return map;
     }
 }
