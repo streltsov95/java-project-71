da9bbf05a49185b22d21d36619359428e9cb2a7b
Viktor Streltsov <streltsov95va@gmail.com>
2024-07-24 16:00:50 +0300

    Refactor format()

--- app/src/main/java/hexlet/code/formatters/PlainFormatter.java
+++ app/src/main/java/hexlet/code/formatters/PlainFormatter.java
@@ -6,6 +6,10 @@ import java.util.Map;
 
 public class PlainFormatter {
 
+    private static final String UPDATED_TEMPLATE = "Property '%s' was updated. From %s to %s\n";
+    private static final String REMOVED_TEMPLATE = "Property '%s' was removed\n";
+    private static final String ADDED_TEMPLATE = "Property '%s' was added with value: %s\n";
+
     private static String getValue(Object data) {
         if (data == null) {
             return "null";
@@ -21,22 +25,25 @@ public class PlainFormatter {
     public static String format(List<Map<String, Object>> compareResult) {
 
         StringBuilder sb = new StringBuilder();
+
         for (Map<String, Object> map : compareResult) {
 
-            String templateUpdated = String.format("Property '%s' was updated. From %s to %s\n",
-                    map.get("FIELD"), getValue(map.get("OLD_VALUE")), getValue(map.get("NEW_VALUE")));
-            String templateRemoved = String.format("Property '%s' was removed\n", map.get("FIELD"));
-            String templateAdded = String.format("Property '%s' was added with value: %s\n",
-                    map.get("FIELD"), getValue(map.get("NEW_VALUE")));
-
-            if (map.get("STATUS").equals("UPDATED")) {
-                sb.append(templateUpdated);
-            }
-            if (map.get("STATUS").equals("REMOVED")) {
-                sb.append(templateRemoved);
-            }
-            if (map.get("STATUS").equals("ADDED")) {
-                sb.append(templateAdded);
+            String status = String.valueOf(map.get("STATUS"));
+            switch (status) {
+                case "UPDATED" -> {
+                    String updated = String.format(UPDATED_TEMPLATE,
+                            map.get("FIELD"), getValue(map.get("OLD_VALUE")), getValue(map.get("NEW_VALUE")));
+                    sb.append(updated);
+                }
+                case "REMOVED" -> {
+                    String removed = String.format(REMOVED_TEMPLATE, map.get("FIELD"));
+                    sb.append(removed);
+                }
+                case "ADDED" -> {
+                    String added = String.format(ADDED_TEMPLATE, map.get("FIELD"), getValue(map.get("NEW_VALUE")));
+                    sb.append(added);
+                }
+                default -> sb.append("");
             }
         }
         return sb.toString().trim();
